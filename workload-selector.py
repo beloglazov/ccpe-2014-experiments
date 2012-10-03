e9cac203a1a54a68ca99ac259a2a2dd605cc4c97
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-10-03 10:53:49 +1000

    Added a workload selector script

---
+++ workload-selector.py
@@ -0,0 +1,42 @@
+# Copyright 2012 Anton Beloglazov
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+import os
+import random
+import shutil
+
+if len(sys.argv) < 3:
+    print 'You must specify 2 arguments:'
+    print '1. The directory containing workload trace files'
+    print '2. The directory to write the selected from to'
+    print '3. The number of workload trace files to select'
+    sys.exit(1)
+
+src = sys.argv[1]
+dest = sys.argv[2]
+n = int(sys.argv[3])
+
+try:
+    os.makedirs(dest)
+except os.error:
+    pass
+
+files = os.listdir(src)
+selected = []
+for i in range(n):
+    shutil.copy(os.path.join(src, random.choice(files)), dest)
+
+print 'Selected and copied ' + str(n) + \
+      ' workload trace files to ' + dest

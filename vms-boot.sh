5d437cec8a74b29a68bec00e7d1236e3dc91655a
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-10-18 16:32:05 +1100

    Updated vms-boot to use the new ubuntu-spe 3 image

--- vms-boot.sh
+++ vms-boot.sh
@@ -2,5 +2,5 @@
 
 for i in {1..8}
 do
-    nova boot --image ubuntu-spe2 --flavor nano384 --key_name test "ubuntu$i"
+    nova boot --image ubuntu-spe3 --flavor pico --key_name test "ubuntu$i"
 done

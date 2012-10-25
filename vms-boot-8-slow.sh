52a038083227605b87080818c77a1aec52b367ff
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-10-26 10:10:05 +1100

    Updated the booting scripts to use ubuntu-spe9

--- vms-boot-8-slow.sh
+++ vms-boot-8-slow.sh
@@ -2,6 +2,6 @@
 
 for i in {1..8}
 do
-    nova boot --image ubuntu-spe8 --flavor pico --key_name test "ubuntu$i"
+    nova boot --image ubuntu-spe9 --flavor pico --key_name test "ubuntu$i"
     sleep 5
 done

658fee11bdb254506a41a8aa4479c0d6fe769f2c
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-10-27 16:22:05 +1100

    Fixed the output redirection

--- start-distributor-full.sh
+++ start-distributor-full.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 rm -f workload-distributor-server.log
-python2 workload-distributor.py full-utilization > workload-distributor-server.log
+python2 workload-distributor.py full-utilization &> workload-distributor-server.log
--- start-distributor-planetlab.sh
+++ start-distributor-planetlab.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 rm -f workload-distributor-server.log
-python2 workload-distributor.py planetlab-selected > workload-distributor-server.log
+python2 workload-distributor.py planetlab-selected &> workload-distributor-server.log

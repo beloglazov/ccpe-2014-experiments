398d57799829099529cf52b71f1a62ff26b94318
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-10-27 16:23:09 +1100

    Added a list of IP addresses

--- workload-receivers.sh
+++ workload-receivers.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-cat workload-distributor-server.log | egrep 'GET.*200' | cut -d ' ' -f 1 | sort -u | wc -l
+cat workload-distributor-server.log | egrep 'GET.*200' | cut -d ' ' -f 1 | sort -u
+
+echo ''
+echo "Total: `cat workload-distributor-server.log | egrep 'GET.*200' | cut -d ' ' -f 1 | sort -u | wc -l`"

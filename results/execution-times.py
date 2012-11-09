36a793b007fbd5e3c212bf6b7726f1930081f626
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-11-09 12:06:02 +1100

    Updated the execution-times script to limit the start and end times

--- results/execution-times.py
+++ results/execution-times.py
@@ -16,20 +16,27 @@ import sys
 import datetime
 from pprint import pprint
 
-if len(sys.argv) < 3:
-    print 'You must specify 2 arguments:'
-    print '1. A list of start times'
-    print '2. A list of end times'
+if len(sys.argv) < 5:
+    print 'You must specify 4 arguments:'
+    print '1. The start time'
+    print '2. The end time'
+    print '3. A list of start times'
+    print '4. A list of end times'
     sys.exit(1)
 
-if not sys.argv[1] or not sys.argv[2]:
+if not sys.argv[3] or not sys.argv[4]:
     sys.exit(0)
 
-start_times = [datetime.datetime.strptime(x, "%Y-%m-%d %H:%M:%S,%f") 
-               for x in sys.argv[1].split('\n')]
-end_times = [datetime.datetime.strptime(x, "%Y-%m-%d %H:%M:%S,%f") 
-             for x in sys.argv[2].split('\n')]
-
+start_time = datetime.datetime.strptime(sys.argv[1], "%Y-%m-%d %H:%M:%S")
+end_time = datetime.datetime.strptime(sys.argv[2], "%Y-%m-%d %H:%M:%S")
+start_times_all = [datetime.datetime.strptime(x, "%Y-%m-%d %H:%M:%S,%f") 
+                   for x in sys.argv[3].split('\n')]
+start_times = [x for x in start_times_all 
+               if x >= start_time and x <= end_time]
+end_times_all = [datetime.datetime.strptime(x, "%Y-%m-%d %H:%M:%S,%f") 
+                 for x in sys.argv[4].split('\n')]
+end_times = [x for x in end_times_all 
+             if x >= start_time and x <= end_time]
 
 def total_microseconds(delta):
     return delta.microseconds + \
@@ -53,7 +60,6 @@ deltas = [total_microseconds(end - start) for start, end in pairs]
 for delta in deltas:
     print float(delta) / 1000000
 
-mean = int(round(float(sum(deltas)) / len(deltas)))
-
-print ''
-print 'Mean: ' + str(mean)
+#mean = int(round(float(sum(deltas)) / len(deltas)))
+#print ''
+#print 'Mean: ' + str(mean)

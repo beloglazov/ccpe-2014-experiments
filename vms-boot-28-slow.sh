f5cdece831eb348acc867068295012c05ee41631
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-11-28 17:34:39 +1100

    Updated the VM image name

--- vms-boot-28-slow.sh
+++ vms-boot-28-slow.sh
@@ -2,6 +2,6 @@
 
 for i in {1..28}
 do
-    nova boot --image spe-ubuntu --flavor pico --key_name test `printf ubuntu%02d $i`
+    nova boot --image spe-ubuntu2 --flavor pico --key_name test `printf ubuntu%02d $i`
     sleep 5
 done
--- vms-boot-8.sh
+++ vms-boot-8.sh
@@ -2,5 +2,5 @@
 
 for i in {1..8}
 do
-    nova boot --image spe-ubuntu --flavor pico --key_name test "ubuntu$i"
+    nova boot --image spe-ubuntu8 --flavor pico --key_name test "ubuntu$i"
 done

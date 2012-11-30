6380613d4fd244467f3d5bc19bee2a2d188454b7
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-11-30 17:32:34 +1100

    Fixed the db-dump script to use the correct database

--- results/do-all.sh
+++ results/do-all.sh
@@ -5,12 +5,14 @@ output2=execution-times.csv
 rm $output1
 rm $output2
 
+#./do-one.sh \
+#    thr-2-0.8.tar.gz '2012-11-21 19:11:21' '2012-11-22 19:11:21' $output1 $output2
+#./do-one.sh \
+#    lr-1.0-30.tar.gz '2012-11-23 12:31:03' '2012-11-24 12:31:03' $output1 $output2
+#./do-one.sh \
+#    mhod-0.01-0.8.tar.gz '2012-11-20 17:19:03' '2012-11-21 17:19:03' $output1 $output2
 ./do-one.sh \
-    thr-2-0.8.tar.gz '2012-11-21 19:11:21' '2012-11-22 19:11:21' $output1 $output2
-./do-one.sh \
-    lr-1.0-30.tar.gz '2012-11-23 12:31:03' '2012-11-24 12:31:03' $output1 $output2
-./do-one.sh \
-    mhod-0.01-0.8.tar.gz '2012-11-20 17:19:03' '2012-11-21 17:19:03' $output1 $output2
+    mhod-0.1-0.8.tar.gz '2012-11-29 11:01:39' '2012-11-30 11:01:39' $output1 $output2
 
 
 #./do-one.sh \

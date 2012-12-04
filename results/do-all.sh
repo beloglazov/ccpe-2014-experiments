e967cff8499109552250eab87a4c9d8cb43c4253
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-12-04 13:24:30 +1100

    Added results of a second run of thr-2-0.8

--- results/do-all.sh
+++ results/do-all.sh
@@ -8,6 +8,8 @@ rm $output2
 ./do-one.sh \
     thr-2-0.8.tar.gz '2012-11-21 19:11:21' '2012-11-22 19:11:21' $output1 $output2
 ./do-one.sh \
+    thr-2-0.8-2.tar.gz '2012-12-03 10:28:24' '2012-12-04 10:28:24' $output1 $output2
+./do-one.sh \
     lr-1.0-30.tar.gz '2012-11-23 12:31:03' '2012-11-24 12:31:03' $output1 $output2
 ./do-one.sh \
     mhod-0.01-0.8.tar.gz '2012-11-20 17:19:03' '2012-11-21 17:19:03' $output1 $output2

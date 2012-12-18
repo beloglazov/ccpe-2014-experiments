262b7b54f73cf60f3b80b514faa1841147d337b6
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-12-18 18:07:02 +1100

    Added mhod-0.10-0.8-500 results (known to be erroneous)

--- results/do-all.sh
+++ results/do-all.sh
@@ -11,15 +11,16 @@ rm $output2
     thr-2-0.8-2.tar.gz '2012-12-03 10:28:24' '2012-12-04 10:28:24' $output1 $output2
 ./do-one.sh \
     lr-1.0-30.tar.gz '2012-11-23 12:31:03' '2012-11-24 12:31:03' $output1 $output2
-./do-one.sh \
-    mhod-0.01-0.8.tar.gz '2012-11-20 17:19:03' '2012-11-21 17:19:03' $output1 $output2
-./do-one.sh \
-    mhod-0.1-0.8.tar.gz '2012-11-29 11:01:39' '2012-11-30 11:01:39' $output1 $output2
-./do-one.sh \
-    mhod-0.2-0.8.tar.gz '2012-12-01 16:55:53' '2012-12-02 16:55:53' $output1 $output2
+# ./do-one.sh \
+#     mhod-0.01-0.8.tar.gz '2012-11-20 17:19:03' '2012-11-21 17:19:03' $output1 $output2
+# ./do-one.sh \
+#     mhod-0.1-0.8.tar.gz '2012-11-29 11:01:39' '2012-11-30 11:01:39' $output1 $output2
+# ./do-one.sh \
+#     mhod-0.2-0.8.tar.gz '2012-12-01 16:55:53' '2012-12-02 16:55:53' $output1 $output2
 
 ./do-one.sh \
     best-idf.tar.gz '2012-12-04 13:50:14' '2012-12-05 13:50:14' $output1 $output2
+
 ./do-one.sh \
     mhod-0.1-0.8-2.tar.gz '2012-12-05 16:09:24' '2012-12-06 16:09:24' $output1 $output2
 ./do-one.sh \
@@ -33,3 +34,6 @@ rm $output2
 
 ./do-one.sh \
     mhod-0.15-0.8-100-1.tar.gz '2012-12-15 12:30:44' '2012-12-16 12:30:44' $output1 $output2
+
+./do-one.sh \
+    mhod-0.10-0.8-500-1.tar.gz '2012-12-17 13:41:27' '2012-12-18 13:41:27' $output1 $output2

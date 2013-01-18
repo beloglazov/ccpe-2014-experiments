7b086d579199cb4c193c5974e4d93da32225c0a1
Anton Beloglazov <anton.beloglazov@gmail.com>
2013-01-18 18:19:04 +1100

    Regenerated the results

--- results/do-all.sh
+++ results/do-all.sh
@@ -5,33 +5,33 @@ output2=execution-times.csv
 rm $output1
 rm $output2
 
-./do-one.sh \
-    thr-2-0.8.tar.gz '2012-11-21 19:11:21' '2012-11-22 19:11:21' $output1 $output2
-#./do-one.sh \
-#    thr-2-0.8-2.tar.gz '2012-12-03 10:28:24' '2012-12-04 10:28:24' $output1 $output2
-./do-one.sh \
-    lr-1.0-30.tar.gz '2012-11-23 12:31:03' '2012-11-24 12:31:03' $output1 $output2
-./do-one.sh \
-    best-idf.tar.gz '2012-12-04 13:50:14' '2012-12-05 13:50:14' $output1 $output2
+# ./do-one.sh \
+#     thr-2-0.8.tar.gz '2012-11-21 19:11:21' '2012-11-22 19:11:21' $output1 $output2
+# #./do-one.sh \
+# #    thr-2-0.8-2.tar.gz '2012-12-03 10:28:24' '2012-12-04 10:28:24' $output1 $output2
+# ./do-one.sh \
+#     lr-1.0-30.tar.gz '2012-11-23 12:31:03' '2012-11-24 12:31:03' $output1 $output2
+# ./do-one.sh \
+#     best-idf.tar.gz '2012-12-04 13:50:14' '2012-12-05 13:50:14' $output1 $output2
 
-./do-one.sh \
-    mhod-0.8-0.10-500-1.tar.gz '2012-12-20 10:59:47' '2012-12-21 10:59:47' $output1 $output2
-./do-one.sh \
-    mhod-0.8-0.10-500-2.tar.gz '2012-12-21 16:05:20' '2012-12-22 16:05:20' $output1 $output2
+# ./do-one.sh \
+#     mhod-0.8-0.10-500-1.tar.gz '2012-12-20 10:59:47' '2012-12-21 10:59:47' $output1 $output2
+# ./do-one.sh \
+#     mhod-0.8-0.10-500-2.tar.gz '2012-12-21 16:05:20' '2012-12-22 16:05:20' $output1 $output2
 
-./do-one.sh \
-    mhod-0.8-0.10-500-0.7-1.tar.gz '2012-12-22 19:09:36' '2012-12-23 19:09:36' $output1 $output2
+# ./do-one.sh \
+#     mhod-0.8-0.10-500-0.7-1.tar.gz '2012-12-22 19:09:36' '2012-12-23 19:09:36' $output1 $output2
 
-./do-one.sh \
-    mhod-0.8-0.20-500-0.7-1.tar.gz '2012-12-24 14:28:33' '2012-12-25 14:28:33' $output1 $output2
+# ./do-one.sh \
+#     mhod-0.8-0.20-500-0.7-1.tar.gz '2012-12-24 14:28:33' '2012-12-25 14:28:33' $output1 $output2
 
-./do-one.sh \
-    mhod-0.8-0.20-500-0.8-1.tar.gz '2012-12-25 18:23:03' '2012-12-26 18:23:03' $output1 $output2
+# ./do-one.sh \
+#     mhod-0.8-0.20-500-0.8-1.tar.gz '2012-12-25 18:23:03' '2012-12-26 18:23:03' $output1 $output2
 
-./do-one.sh \
-    mhod-0.8-0.20-500-0.8-only-n-1.tar.gz '2012-12-27 13:03:28' '2012-12-28 13:03:28' $output1 $output2
-./do-one.sh \
-    mhod-0.8-0.30-500-0.8-only-n-1.tar.gz '2012-12-29 13:55:13' '2012-12-30 13:55:13' $output1 $output2
+# ./do-one.sh \
+#     mhod-0.8-0.20-500-0.8-only-n-1.tar.gz '2012-12-27 13:03:28' '2012-12-28 13:03:28' $output1 $output2
+# ./do-one.sh \
+#     mhod-0.8-0.30-500-0.8-only-n-1.tar.gz '2012-12-29 13:55:13' '2012-12-30 13:55:13' $output1 $output2
 
 ./do-one.sh \
     thr-2-0.8-1.tar.gz '2013-01-07 18:57:57' '2013-01-08 18:57:57' $output1 $output2
@@ -54,5 +54,5 @@ rm $output2
 ./do-one.sh \
     lrr-0.8-1.0-30-3.tar.gz '2013-01-15 12:26:57' '2013-01-16 12:26:57' $output1 $output2
 
-./do-one.sh \
-    lrr-0.8-0.9-30-1.tar.gz '2013-01-16 13:55:32' '2013-01-17 13:55:32' $output1 $output2
+# ./do-one.sh \
+#     lrr-0.8-0.9-30-1.tar.gz '2013-01-16 13:55:32' '2013-01-17 13:55:32' $output1 $output2

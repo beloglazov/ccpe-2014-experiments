58510caa52c75802e1e6bc1ee70fee114a6e17d1
Anton Beloglazov <anton.beloglazov@gmail.com>
2013-04-01 17:11:29 +1100

    Added results thr-1.0-3

--- results/do-all.sh
+++ results/do-all.sh
@@ -148,3 +148,5 @@ rm $output2
      thr-1.0-1.tar.gz '2013-03-25 13:48:19' '2013-03-26 13:48:19' $output1 $output2
 ./do-one.sh \
      thr-1.0-2.tar.gz '2013-03-26 18:30:37' '2013-03-27 18:30:37' $output1 $output2
+./do-one.sh \
+     thr-1.0-3.tar.gz '2013-03-28 10:46:12' '2013-03-29 10:46:12' $output1 $output2

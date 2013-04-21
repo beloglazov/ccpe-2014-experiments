dea8f4a2b39d4c8e7570e56c1691dbde3050d55c
Anton Beloglazov <anton.beloglazov@gmail.com>
2013-04-21 15:21:07 +1000

    Added energy estimations

---
+++ results/do-energy-all.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+echo "THR-0.8"
+./energy.py 0.36879 0.15397
+
+echo "THR-0.9"
+./energy.py 0.43011 0.26929
+
+echo "THR-1.0"
+./energy.py 0.493207 0.4216
+
+echo "LRR-1.1"
+./energy.py 0.379450 0.1775
+
+echo "LRR-1.0"
+./energy.py 0.40283 0.23794
+
+echo "LRR-0.9"
+./energy.py 0.47309 0.3438
+
+echo "MHOD-0.2"
+./energy.py 0.37681 0.16013
+
+echo "MHOD-0.3"
+./energy.py 0.381138 0.17882
+
+echo "MAX-ITF"
+./energy.py 0.492273 0.4035
+
